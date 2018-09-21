6976d690efea67dcbd14c8f57acd745ee1275ddf
tgray017 <tgray017@gmail.com>
2018-09-21 22:31:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
   else
-    katz_deli.each {|customer|
-    puts "The line is currently"
+    katz_deli.each do |customer|
+      place_in_line = customer.index
+      puts "The line is currently"
+    end
   end
 end
 
