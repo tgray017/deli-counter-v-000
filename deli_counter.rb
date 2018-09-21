223dadf965861c3d5b69b2b9aff273a39653fbee
tgray017 <tgray017@gmail.com>
2018-09-21 22:29:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  
+  if katz_deli.empty?
+    puts "The line is currently empty"
+  else
+    katz_deli.each {|customer|
+    puts "The line is currently"
+  end
 end
 
 def take_a_number(katz_deli, last_customer)
