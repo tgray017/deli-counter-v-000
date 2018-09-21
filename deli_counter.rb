7a17a0bdd45b28cea48de0821e188c198c6bf307
tgray017 <tgray017@gmail.com>
2018-09-21 22:23:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,14 @@ katz_deli = []
 def line(katz_deli)
   
 end
+
+def take_a_number(katz_deli, last_customer)
+  
+  
+  puts "Welcome, #{last_customer}. You are number #{} in line"
+end
+
+def now_serving()
+  puts 
+  katz_deli.unshift
+end
