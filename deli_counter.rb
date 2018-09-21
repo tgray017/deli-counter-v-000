cfa70628db50c64780ee6b62796da48b37024c71
tgray017 <tgray017@gmail.com>
2018-09-21 22:37:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,18 +6,27 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty"
   else
+    line_array = []
     katz_deli.each do |customer|
       place_in_line = customer.index + 1
-      puts "The line is currently: "
+      "#{place_in_line}. #{customer} " << line_array
     end
+    puts "The line is currently: "
   end
 end
+
+
+
 
 def take_a_number(katz_deli, last_customer)
   
   
   puts "Welcome, #{last_customer}. You are number #{} in line"
 end
+
+
+
+
 
 def now_serving()
   puts 
